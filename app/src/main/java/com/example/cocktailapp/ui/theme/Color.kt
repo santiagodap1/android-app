f54b397cf6b903b5547d94ead6c6392a8f58941a
santi <santiagodeabreu430@gmail.com>
2025-11-11 00:00:35 +0000

    test

--- app/src/main/java/com/example/cocktailapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/cocktailapp/ui/theme/Color.kt
@@ -11,6 +11,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+//background gradient
 val DarkGradient = Brush.verticalGradient(
     colors = listOf(Color(0xFF222222), Color.Black)
 )
